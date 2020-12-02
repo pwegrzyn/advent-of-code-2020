d376e428727b38daf4a62b59b63d8ce25990e6bc
Patryk Węgrzyn <wegpat@gmail.com>
2020-12-02 15:57:14 +0100

    Add day2

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # advent-of-code-2020
-Solutions to AoC 2020 in 5 languages
+Solutions to AoC 2020 in:
+* Python
+* Go
+* TypeScript
+* Scala
+* Rust
--- day2/sol.go
+++ day2/sol.go
@@ -39,6 +39,40 @@ func sol1(lines []string) int {
 	return validPasswordsCount
 }
 
+func sol2(lines []string) int {
+	validPasswordsCount := 0
+	for _, line := range lines {
+		splitByColon := strings.Split(line, ":")
+		splitBySpace := strings.Split(splitByColon[0], " ")
+		specialChar := splitBySpace[1]
+		splitByDash := strings.Split(splitBySpace[0], "-")
+		firstOccurence, errMinOcc := strconv.Atoi(splitByDash[0])
+		secondOccurence, errMaxOcc := strconv.Atoi(splitByDash[1])
+		if errMinOcc != nil || errMaxOcc != nil {
+			log.Fatal("Wrong number format!")
+		}
+		password := splitByColon[1][1:len(splitByColon[1])]
+
+		seenChar := false
+		for idx, char := range password {
+			if specialChar == fmt.Sprintf("%c", char) {
+				if idx + 1 == firstOccurence || idx + 1 == secondOccurence {
+					if seenChar {
+						// if we have already seen the special char then this password is invalid
+						seenChar = false
+						break
+					}
+					seenChar = true
+				}
+			}
+		}
+		if seenChar {
+			validPasswordsCount++
+		}
+	}
+	return validPasswordsCount
+}
+
 func main() {
 	file, err := os.Open(inputPath)
 	if err != nil {
@@ -56,5 +90,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(sol1(lines))
+	fmt.Println(sol2(lines))
 }
